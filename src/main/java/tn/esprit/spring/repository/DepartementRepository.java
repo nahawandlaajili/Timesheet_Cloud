5dbabe74421e1ece2572052bdc69eb6ea1b3a015
326-163 <nahawand.laajili@gmail.com>
2025-05-29 09:36:54 +0200

    FDF

--- src/main/java/tn/esprit/spring/repository/DepartementRepository.java
+++ src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.Departement;
-
+// COOMMIT TEST
 @Repository
 public interface DepartementRepository extends CrudRepository<Departement, Integer>{
 
