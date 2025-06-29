af3b92c42571e288de136718f8adf40854d84f41
326-163 <nahawand.laajili@gmail.com>
2025-06-29 19:30:58 +0200

    deployment added

--- app/src/main/java/tn/esprit/spring/entities/Mission.java
+++ app/src/main/java/tn/esprit/spring/entities/Mission.java
@@ -6,15 +6,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;   
+import jakarta.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
--- app/src/main/java/tn/esprit/spring/repository/DepartementRepository.java
+++ app/src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -11,11 +11,3 @@ import tn.esprit.spring.entities.Departement;
 @Repository
 public interface DepartementRepository extends JpaRepository<Departement, Integer> {
 
-    // Additional custom queries can be declared here if needed
-
-}
-// This interface allows for basic CRUD operations on Departement entities
-// without the need for boilerplate code. It uses Spring Data JPA's
-// JpaRepository, which provides methods like save, findById, findAll, deleteById, etc.
-// The @Repository annotation indicates that this interface is a Spring Data repository,
-// enabling exception translation and other repository-related features.    
