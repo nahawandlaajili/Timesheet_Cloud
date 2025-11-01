d4b5b7a5df4d77a574e43475c6d58c629fd94482
326-163 <nahawand.laajili@gmail.com>
2025-11-01 23:14:12 +0100

    other config

--- app/timesheet-frontend/src/signup.js
+++ app/timesheet-frontend/src/signup.js
@@ -25,7 +25,7 @@ function SignupPage() {
         setMessage("");
 
         try {
-            const response = await fetch("http://localhost:7070/auth/signup", {
+            const response = await fetch("http://localhost:8080/auth/signup", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
