d14b2e41ffb014b8e2081fa7fd3e394c713d3082
326-163 <nahawand.laajili@gmail.com>
2025-11-02 15:20:10 +0100

    user serv fixed

--- app/user-service/src/main/java/com/cloud/userservice/model/UserProfile.java
+++ app/user-service/src/main/java/com/cloud/userservice/model/UserProfile.java
@@ -1,4 +1,3 @@
-// user-profile-service/src/main/java/com/cloud/userservice/model/UserProfile.java
 package com.cloud.userservice.model;
 
 import jakarta.persistence.*;
@@ -8,7 +7,6 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Table(name = "users")
 public class UserProfile {
 
@@ -18,7 +16,6 @@ public class UserProfile {
 
     @Column(nullable = false, unique = true)
     private String name; 
-
 
     private String email;
 
@@ -40,11 +37,111 @@ public class UserProfile {
     @Enumerated(EnumType.STRING)
     private ContractType contractType;
 
+    // Manual getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public int getRemainingDaysOff() {
         return remainingDaysOff;
     }
 
     public void setRemainingDaysOff(int remainingDaysOff) {
         this.remainingDaysOff = remainingDaysOff;
+    }
+
+    // Manual builder implementation
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String email;
+        private String phone;
+        private String address;
+        private int remainingDaysOff = 30;
+        private String password;
+        private Department department;
+        private JobRole jobRole;
+        private ContractType contractType;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder remainingDaysOff(int remainingDaysOff) {
+            this.remainingDaysOff = remainingDaysOff;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder department(Department department) {
+            this.department = department;
+            return this;
+        }
+
+        public Builder jobRole(JobRole jobRole) {
+            this.jobRole = jobRole;
+            return this;
+        }
+
+        public Builder contractType(ContractType contractType) {
+            this.contractType = contractType;
+            return this;
+        }
+
+        public UserProfile build() {
+            UserProfile userProfile = new UserProfile();
+            userProfile.id = this.id;
+            userProfile.name = this.name;
+            userProfile.email = this.email;
+            userProfile.phone = this.phone;
+            userProfile.address = this.address;
+            userProfile.remainingDaysOff = this.remainingDaysOff;
+            userProfile.password = this.password;
+            userProfile.department = this.department;
+            userProfile.jobRole = this.jobRole;
+            userProfile.contractType = this.contractType;
+            return userProfile;
+        }
     }
 }
